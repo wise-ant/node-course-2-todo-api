9bd0e2043a86f40cf23fd914efe25c555352e12b
Antonina Mashtakova <antonina.mashtakova415@gmail.com>
2018-05-06 23:55:50 -0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -10,13 +10,21 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  console.log(req.body);
+  //console.log(req.body);
   const todo = new Todo({
     text: req.body.text
   });
 
   todo.save().then( (doc) => {
     res.send(doc);
+  }, (e) => {
+    res.status(400).send(e)
+  })
+})
+
+app.get('/todos', (req, res) => {
+  Todo.find().then(todos => {
+    res.send(todos)
   }, (e) => {
     res.status(400).send(e)
   })
